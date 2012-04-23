cdf329c800ca207433d3c08592bdcec5ef9fa09a
Andrew Tomaka <atomaka@atomaka3.(none)>
2012-04-22 22:15:31 -0400

    Initial commit of Ruby on Rails framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwWhoisandrewCom::Application.config.secret_token = '96d010a32a495fb1286684abbe123c0c9e28a929143806344ef15be9c05ca5f13177a8d347328dafe5283d969223426806c756535288a3985acc95a794ed6691'
