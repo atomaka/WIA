56f2b7348ec2ad9dcf8324ef888f7a45acaa7b99
Andrew Tomaka <atomaka@gmail.com>
2012-05-03 01:25:26 -0400

    Add URL validation class.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 WwwWhoisandrewCom::Application.initialize!
+
+# Include the URL validator
+require 'url_validator'
